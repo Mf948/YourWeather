81de09613069b2c30365ee824fa5709f20a5d622
Mateo Ferreira <matteo948@icloud.com>
2020-12-14 20:57:27 -0500

    script,API,DisplayElement

---
+++ .history/script_20201214194050.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+console.log('ready!')
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+});
---
+++ .history/script_20201214194055.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+});
